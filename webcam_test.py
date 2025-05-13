4aaf93f50bb4e2300b08863997353c269d97585a
Yu-Wen Wang <drwang.nini@gmail.com>
2025-05-12 22:43:28 -0700

    Update webcam_test.py

--- webcam_test.py
+++ webcam_test.py
@@ -1,6 +1,6 @@
 import cv2
 
-cap = cv2.VideoCapture(0)  # 0 是你的內建 webcam
+cap = cv2.VideoCapture(0)  
 
 while True:
     ret, frame = cap.read()
@@ -9,7 +9,6 @@ while True:
 
     cv2.imshow("Webcam Feed", frame)
 
-    # 按 q 鍵結束
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
